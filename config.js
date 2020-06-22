ba3da8d00d709e9f28120d5ed6246ed39ca5a963
Max Goodman <max@maxgrok.com>
2020-06-22 12:16:11 -0400

    changing name to see if it posts

--- config.js
+++ config.js
@@ -1,14 +1,14 @@
 'use strict';
 
 module.exports = {
-  url: 'https://lumen.netlify.com',
+  url: 'https://maxgrok.com',
   pathPrefix: '/',
-  title: 'Blog by John Doe',
-  subtitle: 'Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu.',
+  title: 'Max\'s Blog',
+  subtitle: 'Engineer.',
   copyright: '© All rights reserved.',
   disqusShortname: '',
   postsPerPage: 4,
-  googleAnalyticsId: 'UA-73379983-2',
+  googleAnalyticsId: '',
   useKatex: false,
   menu: [
     {
@@ -25,25 +25,25 @@ module.exports = {
     }
   ],
   author: {
-    name: 'John Doe',
+    name: 'Max Goodman',
     photo: '/photo.jpg',
-    bio: 'Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu.',
+    bio: 'Engineer.',
     contacts: {
-      email: '',
-      facebook: '#',
-      telegram: '#',
-      twitter: '#',
-      github: '#',
-      rss: '',
-      vkontakte: '',
-      linkedin: '#',
-      instagram: '#',
-      line: '',
-      gitlab: '',
-      weibo: '',
-      codepen: '',
-      youtube: '',
-      soundcloud: '',
+      email: 'max@maxgrok.com',
+      // facebook: '#',
+      telegram: '@maxgrok',
+      twitter: '@maxxgrok',
+      github: '@maxgrok',
+      // rss: '',
+      // vkontakte: '',
+      // linkedin: '#',
+      // instagram: '#',
+      // line: '',
+      // gitlab: '',
+      // weibo: '',
+      // codepen: '',
+      // youtube: '',
+      // soundcloud: '',
     }
   }
 };
